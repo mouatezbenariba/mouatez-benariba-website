3c77484d9a579d222f5681af34213f9a418dc31a
Mouatez Benariba <benariba.mouatez@gmail.com>
2023-04-11 16:56:49 +0100

    remove script tag

--- src/js/google-analytics.js
+++ src/js/google-analytics.js
@@ -1,8 +1,6 @@
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag('js', new Date());
-      gtag('config', 'G-EXP449HEZ5');
-    </script>
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-EXP449HEZ5');
